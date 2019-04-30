4a1e42d0ac2a1d2e9e7df09f7e789e8e96d33b75
dattebayoRob <robson.william65@gmail.com>
2019-04-29 23:39:14 -0300

    Go away from zalando

--- src/main/java/com/dtb/restapi/config/ApiConfig.java
+++ src/main/java/com/dtb/restapi/config/ApiConfig.java
@@ -1,22 +1,13 @@
 package com.dtb.restapi.config;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.zalando.problem.ProblemModule;
-import org.zalando.problem.validation.ConstraintViolationProblemModule;
 
 @Configuration
 public class ApiConfig {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
-	}
-
-	@Bean
-	public Jackson2ObjectMapperBuilderCustomizer problemObjectMapperModules() {
-		return jacksonObjectMapperBuilder -> jacksonObjectMapperBuilder.modules(new ProblemModule(),
-				new ConstraintViolationProblemModule());
 	}
 }
