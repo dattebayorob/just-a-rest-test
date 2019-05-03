6b9788bda7fa8f094367b24090b6760a8b7e2b46
dattebayoRob <robson.william65@gmail.com>
2019-05-02 22:20:36 -0300

    Update vavr; Fix wrong messages.properties path; Add more rows on db

--- src/main/resources/db/migration/JustASqlScript.sql
+++ src/main/resources/db/migration/JustASqlScript.sql
@@ -1,1 +1,47 @@
-INSERT INTO entity(name,cpf,rg,date,enabled) VALUES('Just a Entity','cpf1','rg1',CURRENT_TIME,true),('Just a Entity 2','cpf2','rg2',CURRENT_TIME,true),('Just a Entity 3','cpf3','rg3',CURRENT_TIME,false);
+INSERT INTO entity(name,cpf,rg,date,enabled) 
+VALUES('Just a Entity','cpf1','rg1',CURRENT_TIME,true),
+('Just a Entity 2','cpf22','rg2',CURRENT_TIME,true),
+('Just a Entity 3','cpf222','rg32',CURRENT_TIME,true),
+('Just a Entity 4','cpf332','rg42',CURRENT_TIME,true),
+('Just a Entity 5','cpf442','rg52',CURRENT_TIME,true),
+('Just a Entity 6','cpf4452','r6g2',CURRENT_TIME,true),
+('Just a Entity 7','cggpf2','rg27',CURRENT_TIME,true),
+('Just a Entity 8','cphhf2','rg28',CURRENT_TIME,true),
+('Just a Entity 9','cpkkf2','rg29',CURRENT_TIME,true),
+('Just a Entity 12','cjjpf2','rg102',CURRENT_TIME,true),
+('Just a Entity 22','cllpf2','rg211',CURRENT_TIME,true),
+('Just a Entity 32','cpppf2','rg212',CURRENT_TIME,true),
+('Just a Entity d2','cpfhhh2','rg222',CURRENT_TIME,true),
+('Just a Entity da2','cp333f2','rg2hh',CURRENT_TIME,true),
+('Just a Entity dsa2','cp777f2','rgggg2',CURRENT_TIME,true),
+('Just a Entity d2','cpf99902','rgxxx2',CURRENT_TIME,true),
+('Just a Entity 2','cpfnnmm2','rgvvvv2',CURRENT_TIME,true),
+('Just a Entitysdas 2','cpuuiif2','rglkk2',CURRENT_TIME,true),
+('Just a Entitsdasy 2','c0000pf2','rgççç2',CURRENT_TIME,true),
+('Just a Entidsdty 2','cpfddsss2','rg~~2',CURRENT_TIME,true),
+('Just a sssEntity 2','cpf2aaaq','rg8882',CURRENT_TIME,true),
+('Just a Entssity 2','cpfghh2','rg2111',CURRENT_TIME,true),
+('Just a Entifty 2','cbbbbbpf2','rgfdfsdfsd2',CURRENT_TIME,true),
+('Just a Entddity 2','cpdddddf2','rgcsd2',CURRENT_TIME,true),
+('Just a Entitccy 2','cpfnnnnnnn2','rhjhjg2',CURRENT_TIME,true),
+('Just a E66ntity 2','cp,,,,,f2','rglklk2',CURRENT_TIME,true),
+('Just a Entdcgfcttity 2','c....pf2','rhghghgg2',CURRENT_TIME,true),
+('Just a Entity fd2','cpf2pppp','rg2oooo',CURRENT_TIME,true),
+('Just a Entity 2sfd','cpf28888','rg88772',CURRENT_TIME,true),
+('Just sdfsda Entity 2','cpf0000002','rgnnmbnm2',CURRENT_TIME,true),
+('Just a Entfsfsddfity 2','cpffsdfsdf2','rcxdvg2',CURRENT_TIME,true),
+('Just a Entiterrawrry 2','cpfjhgjghj2','rgkjkj2',CURRENT_TIME,true),
+('Just a Entityrwerw 2','cpfggdfgf2','rgdsdads2',CURRENT_TIME,true),
+('Just a Entiwrwety 2','asjdashdkjsa','rgvbvbv2',CURRENT_TIME,true),
+('Just a Entidsty 2','dhjdhskjdhsa','rgjhjh2',CURRENT_TIME,true),
+('Just a Entitdddy 2','dhskjdhaksj','rgkiiu2',CURRENT_TIME,true),
+('Just a Enti1111ty 2','hdhasj','rgfdfdf2',CURRENT_TIME,true),
+('Just a Entity aaa2','gggg','rg11112',CURRENT_TIME,true),
+('Just a Enti444444ty 3','fakljakjaka','rg9993',CURRENT_TIME,false)
+
+
+
+
+
+
+;
